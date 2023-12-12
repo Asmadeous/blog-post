a1cf4810dc072c5a0f2812b026778c602b753a1c
Ian Ndegwa <Iansky3311@gmail.com>
2023-12-12 15:30:59 +0300

    Added link to titles

--- client/src/features/posts/PostsList.jsx
+++ client/src/features/posts/PostsList.jsx
@@ -1,5 +1,6 @@
 import  { useState, useEffect} from "react";
-import { API_URL } from "../../constants"
+import { API_URL } from "../../constants";
+import { Link } from "react-router-dom";
 
 function PostsList () {
     const [posts, setPosts] = useState([]);
@@ -30,7 +31,11 @@ function PostsList () {
         <div>
             {posts.map((post) => (
                 <div key={post.id} className="post-container">
-                    <h2>{post.title}</h2>
+                    <h2>
+                        <Link to={`/posts/${post.id}`}>
+                        {post.title}
+                        </Link>
+                        </h2>
                     <p>{post.body}</p> 
                     </div>
             ))}
