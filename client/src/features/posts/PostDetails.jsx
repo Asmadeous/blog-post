98798bf57df7d0ce0b274bf70f1a1a9a05b9f65c
Ian Ndegwa <Iansky3311@gmail.com>
2023-12-13 16:14:23 +0300

    Refactored into service and added functionalityy for delete and edit refactored show and index service instead of replicating fetch

--- client/src/features/posts/PostDetails.jsx
+++ client/src/features/posts/PostDetails.jsx
@@ -1,32 +1,42 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
-import { API_URL } from "../../constants";
+import { deletePost, fetchPost } from "../../services/PostService";
 function PostDetails() {
     const [post, setPost] = useState([]);
     const { id } = useParams();
+    const navigate = useNavigate()
 
     useEffect (() => {
         const fetchCurrentPost = async () => {
             try {
-                const response = await fetch(`${API_URL}/${id}`);
-                if (response.ok) {
-                    const json = await response.json();
-                    setPost(json);
-                    // console.log(post)
-                }else {
-                    throw response;
-                }
+                const post = await fetchPost(id)
+                setPost(post)
             } catch(e) {
                 console.log("An error occured:", e)
             }
         };
         fetchCurrentPost();
     }, [id]);
+
+    const deletePosts = async () => {
+        try {
+            await deletePost(post.id);
+            navigate("/")
+        }
+        catch (e) {
+            console.error("Failed to delete the post:", e)
+        }
+    }
+
     return (
         <div>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
+            <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+            {" | "}
             <Link to="/">Back to Posts</Link>
+            {" | "}
+            <button onClick={deletePosts}>Delete</button>
         </div>
     )
 }
